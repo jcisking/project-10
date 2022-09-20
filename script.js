2842b23fc931824816c8e3235478448326513d0b
jcisking <krisloeffler@gmail.com>
2022-09-19 22:55:39 -0700

    added JS and modified css and html

---
+++ script.js
@@ -0,0 +1,9 @@
+let answerParagraphs = document.querySelectorAll(".answerParagraph")
+let arrowIcons = document.querySelectorAll(".arrowIcon")
+let buttons = document.getElementsByClassName("questionAndArrowContainer")
+for (let i = 0; i < buttons.length; i++)
+    buttons[i].addEventListener("click", () => {
+        answerParagraphs[i].classList.toggle("show");
+        arrowIcons[i].classList.toggle("invertIcon")
+        buttons[i].classList.toggle("boldAndDarkText");
+    })
