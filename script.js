597d8fa28ba05cf7a825c32611013ec34fffdfb0
jcisking <krisloeffler@gmail.com>
2022-10-06 15:23:51 -0700

    modified js, css, and html

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-let answerParagraphs = document.querySelectorAll(".answerParagraph")
-let arrowIcons = document.querySelectorAll(".arrowIcon")
-let buttons = document.getElementsByClassName("questionAndArrowContainer")
-for (let i = 0; i < buttons.length; i++) {
-
-    buttons[i].addEventListener("click", () => {
-        answerParagraphs[i].classList.toggle("show");
-        arrowIcons[i].classList.toggle("invertIcon")
-        buttons[i].classList.toggle("boldAndDarkText");
-        buttons[i].classList.toggle("margin");
-        
-    })
-}
+document.addEventListener("click", (e) => {
+    const button = e.target.closest("[data-button]")
+    if (button)
+        button.toggleAttribute("data-show")
+})
